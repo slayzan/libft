1771e0391090c2bd7788b5fe2294eabef323c9cc
Hugo MARQUET <humarque@e1r10p6.42.fr>
2018-11-28 20:16:45 +0100

    dss

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: humarque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 13:24:47 by humarque          #+#    #+#             */
-/*   Updated: 2018/11/28 19:21:15 by humarque         ###   ########.fr       */
+/*   Updated: 2018/11/28 19:31:57 by humarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,21 +37,25 @@ static int		ft_findend(char const *s)
 	return (i);
 }
 
-char	*ft_strbuild(char *s ,int start, int end)
+char	*ft_strbuild(char const *s ,int start, int end)
 {
 	int i;
+	char *s1;
 
 	i = 0;
+	if(!(s1 = (char *)malloc(sizeof(char) * ((end - start) + 2))))
+		return (NULL);
 	while (start <= end)
 	{
-		str[i] = s[start];
+		s1[i] = s[start];
 		i++;
 		start++;
 	}
-	str[i] = '\0';
-	return (str);
+	s1[i] = '\0';
+	return (s1);
 
 }
+
 char			*ft_strtrim(char const *s)
 {
 	char		*str;
@@ -68,19 +72,10 @@ char			*ft_strtrim(char const *s)
 		i = 0;
 		if (start == len && end == 0)
 			start = end + 1;
-		while (s[i])
-			i++;
 		i = len;
 		if (!(str = (char *)malloc(sizeof(char) * ((end - start) + 2))))
 			return (NULL);
-		i = 0;
-		while (start <= end)
-		{
-			str[i] = s[start];
-			i++;
-			start++;
-		}
-		str[i] = '\0';
+		str = ft_strbuild(s, start, end);
 		return (str);
 	}
 	return (NULL);
